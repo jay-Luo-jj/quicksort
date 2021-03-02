35f9e95a4c23c1f6c97b54fdab09ff4f7ec70064
Harry Tedder <harry.tedder@student.manchester.ac.uk>
2021-03-02 15:28:05 +0000

    stuff for the browse page and items table

---
+++ database/migrations/2021_03_02_100438_create_items_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id('item_id');
+            $table->string('name', 64)->nullable();
+            $table->string('description', 2000)->nullable();
+            $table->integer('rating');
+            $table->string('photo', 320)->nullable();
+            $table->foreignId('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
